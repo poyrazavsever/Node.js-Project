66cdf0ef4ca3423f280f0cd8412c3a440af16ba7
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-23 11:59:36 +0300

    feat: learning express.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Merhaba')
+})
+
+app.get('/api/products', (req, res) => {
+    res.send('Ürünler')
+})
+
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})

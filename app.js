b60c335780b3c37e0233254b20dcd3b2eb641917
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-23 12:28:29 +0300

    lr:body-parser

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <html>
+            <head><title>Add a new Product </title> </head>
+
+
+            <body>
+                <form action="product" method="POST">
+                    <input type="text" name="productName" />
+                    <input type="submit" value="Save Product" />
+                </form>
+            </body>
+
+
+        </html>
+        `)
+});
+
+app.post('/product', (req, res, next) => {
+
+    console.log(req.body);
+    
+    res.redirect("/")
+
+})
 
 
 app.use('/',(req, res, next) => {
-    console.log('loglama yapıldı...')
-    next()
+    res.send('<h1>Ana Sayfa</h1>')
 })
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Ürün Ekle</h1>')
-});
-
-app.use('/product-list', (req, res, next) => {
-    res.send('<h1>Ürün Listesi</h1>')
-});
 
 app.listen(3000, () => {
     console.log("listening on port 3000")

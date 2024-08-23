9a1485f1218de3dfc0ddd50898aee689492dbe6c
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-23 22:30:08 +0300

    feat: add new product process

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+
+const products = [
+    {name: "Samsung S8", price:3000, image:"1.jpg", description:'Güzel bir telefon'},
+    {name: "Samsung S4", price:2000, image:"2.jpg", description:'idare telefon'},
+    {name: "Samsung S5", price:1000, image:"3.jpg", description:'fena değil telefon'},
+    {name: "Apple Iphone8", price:5000, image:"4.jpg", description:'Çok Güzel bir telefon'}
+]
 
 router.get('/add-product', (req, res, next) => {
     res.render('add-product', {title: 'Add a New Product'})
@@ -9,10 +15,12 @@ router.get('/add-product', (req, res, next) => {
 router.post('/add-product', (req, res, next) => {
 
     console.log(req.body);
+    products.push({name: req.body.name, price:req.body.price, image: req.body.image, description: req.body.description});
     
     res.redirect("/")
 
 })
 
 
-module.exports = router 
+exports.routes = router 
+exports.products = products
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router()
 const path = require('path');
+const admin = require('./admin')
 
 router.get('/',(req, res, next) => {
 
-    const products = [
-        {name: "Samsung S8", price:3000, image:"1.jpg", description:'Güzel bir telefon'},
-        {name: "Samsung S4", price:2000, image:"1.jpg", description:'idare telefon'},
-        {name: "Samsung S5", price:1000, image:"1.jpg", description:'fena değil telefon'},
-        {name: "Apple Iphone8", price:5000, image:"1.jpg", description:'Çok Güzel bir telefon'}
-    ]
-
-    res.render('index', {title: 'Ana Sayfa', products: products})
+    res.render('index', {title: 'Ana Sayfa', products: admin.products})
 })
 
-module.exports = router
+module.exports = router 

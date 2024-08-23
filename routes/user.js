6972040f6e3ee94a9c2bdd5a07385eeb011593cf
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-23 20:46:54 +0300

    feat: added dynamic content for product card

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,15 @@ const router = express.Router()
 const path = require('path');
 
 router.get('/',(req, res, next) => {
-    res.render('index', {title: 'Ana Sayfa'})
+
+    const products = [
+        {name: "Samsung S8", price:3000, image:"1.jpg", description:'Güzel bir telefon'},
+        {name: "Samsung S4", price:2000, image:"1.jpg", description:'idare telefon'},
+        {name: "Samsung S5", price:1000, image:"1.jpg", description:'fena değil telefon'},
+        {name: "Apple Iphone8", price:5000, image:"1.jpg", description:'Çok Güzel bir telefon'}
+    ]
+
+    res.render('index', {title: 'Ana Sayfa', products: products})
 })
 
 module.exports = router

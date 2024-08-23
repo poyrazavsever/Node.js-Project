8cdf63b0d4b7cb67b2d0091b19017c0fbffc25dd
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-23 12:17:24 +0300

    lr: middleware

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Merhaba')
+app.use('/',(req, res, next) => {
+    console.log('loglama yapıldı...')
+    next()
 })
 
-app.get('/api/products', (req, res) => {
-    res.send('Ürünler')
-})
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Ürün Ekle</h1>')
+});
 
+app.use('/product-list', (req, res, next) => {
+    res.send('<h1>Ürün Listesi</h1>')
+});
 
 app.listen(3000, () => {
     console.log("listening on port 3000")

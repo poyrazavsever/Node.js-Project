f0ae09a4f5d62d2d3db6039e066a2d97e537e346
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-23 19:15:30 +0300

    feat: created public file

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const adminRoutes = require("./routes/admin")
 const userRoutes = require("./routes/user")
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.use('/admin',adminRoutes)
 app.use(userRoutes)

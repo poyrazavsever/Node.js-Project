2b23e719fece1c8c5ad2574a32d2365ae34786ab
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-24 15:53:59 +0300

    feat: done admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,29 +1,41 @@
 const Product = require('../models/product')
 
-// exports.getProducts = (req, res, next) => {
+exports.getProducts = (req, res, next) => {
 
-//     const products = Product.getAll();
+    const products = Product.getAll();
 
-//     res.render('index', 
-//         {
-//         title: 'Ana Sayfa', 
-//         products: products,
-//         path: '/'
-//     })
-// }
+    res.render('admin/products', 
+        {
+        title: 'Admin Products', 
+        products: products,
+        path: '/admin/products'
+    })
+}
 
-// exports.postAddProduct = (req, res, next) => {
-//     const product = new Product(
-//         req.body.name,
-//         req.body.price, 
-//         req.body.imageUrl, 
-//         req.body.description);
+
+exports.getAddProducts =  (req, res, next) => { 
+    res.render('admin/add-product', {title: 'Add Product', path:'/admin/add-product'})
+} 
+
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(
+        req.body.name,
+        req.body.price, 
+        req.body.imageUrl, 
+        req.body.description);
     
-//     product.saveProduct();
+    product.saveProduct();
 
-//     res.redirect('/')
-// } 
+    res.redirect('/')
+} 
 
-// exports.getAddProducts =  (req, res, next) => { 
-//     res.render('add-product', {title: 'Add a New Product', path:'/admin/add-product'})
-// } 
+
+exports.getEditProduct =  (req, res, next) => { 
+    res.render('admin/edit-product', {title: 'Edit Product', path:'/admin/edit-product'})
+} 
+
+
+exports.postEditProduct = (req, res, next) => {
+    res.redirect('/')
+} 
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-// const productsController = require('../controllers/products')
+const adminController = require('../controllers/admin')
 
-// router.get('/add-product', productsController.getAddProducts); 
+router.get('/add-product', adminController.getAddProducts); 
 
-// router.post('/add-product', productsController.postAddProduct)
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product', adminController.getEditProduct); 
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.get('/product', adminController.getProducts); 
+
 
 
 module.exports = router 

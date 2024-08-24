0f73a9cacc314045ca755ef34d3321c1c0c17210
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-24 15:04:58 +0300

    feat: created models

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,8 @@
-
-const products = [
-    {name: "Samsung S8", price:3000, image:"1.jpg", description:'Güzel bir telefon'},
-    {name: "Samsung S4", price:2000, image:"2.jpg", description:'idare telefon'},
-    {name: "Samsung S5", price:1000, image:"3.jpg", description:'fena değil telefon'},
-    {name: "Apple Iphone8", price:5000, image:"4.jpg", description:'Çok Güzel bir telefon'}
-]
+const Product = require('../models/product')
 
 exports.getProducts = (req, res, next) => {
+
+    const products = Product.getAll();
 
     res.render('index', 
         {
@@ -17,8 +13,14 @@ exports.getProducts = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    console.log(req.body);
-    products.push({name: req.body.name, price:req.body.price, image: req.body.image, description: req.body.description});
+    const product = new Product(
+        req.body.name,
+        req.body.price, 
+        req.body.imageUrl, 
+        req.body.description);
+    
+    product.saveProduct();
+
     res.redirect('/')
 } 
 
